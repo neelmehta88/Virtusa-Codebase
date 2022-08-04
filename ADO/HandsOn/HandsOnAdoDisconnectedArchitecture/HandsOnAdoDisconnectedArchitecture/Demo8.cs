3a47ca57c00bb5716fcc9b83c62a7cadca339b0c
sandotnet <87011674+sandotnet@users.noreply.github.com>
2022-08-04 15:51:26 +0530

    new_commit

---
+++ ADO/HandsOn/HandsOnAdoDisconnectedArchitecture/HandsOnAdoDisconnectedArchitecture/Demo8.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace HandsOnAdoDisconnectedArchitecture
+{
+    class Demo8
+    {
+        static void Main()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=SANTU\MSSQLSERVER2019;Initial Catalog=Training1DB;Integrated Security=True");
+            SqlDataAdapter da = null;
+            da = new SqlDataAdapter("Select * from Product", con);
+            DataSet ds = new DataSet("Products");
+            da.Fill(ds, "Product");
+            var resultset = ds.Tables["Product"].AsEnumerable();
+            //get products whose price >300
+            var result1 = from e in resultset
+                          where e.Field<int>("Price") > 300
+                          select e;
+            foreach(var row in result1)
+            {
+                Console.WriteLine($"{row["Pid"]} {row["Pname"]} {row["Price"]} {row["Stock"]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ADO/HandsOn/HandsOnAdo_Demo1/HandsOnAdo_Demo1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace HandsOnAdo_Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=SANTU\MSSQLSERVER2019;Initial Catalog=Training1DB;Integrated Security=True");
+            try
+            {
+                //con.ConnectionString = @"Data Source=SANTU\MSSQLSERVER2019;Initial Catalog=Training1DB;Integrated Security=True";
+                con.Open(); //open connection
+                SqlCommand cmd = new SqlCommand("Select * from Product", con);
+                SqlDataReader dr = cmd.ExecuteReader(); //ExecureReader() method stores result set data into DataReader object
+                con.Close();
+                while (dr.Read())
+                {
+                    
+                    Console.WriteLine("Id:{0} Name:{1} Price {2} Stock {3}", dr["Pid"], dr["Pname"], dr["Price"], dr[3]);
+                    
+                }
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();//close connection i.e disconnected from database
+            }
+        }
+    }
+}
